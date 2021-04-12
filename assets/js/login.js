45f7aca5909d7869d4485a4853e6c629a97deae5
王晨冰 <1554867368@qq.com>
2021-04-12 10:10:46 +0800

    登陆页面

--- assets/js/login.js
+++ assets/js/login.js
@@ -9,7 +9,9 @@ $(function () {
         $('.login-box').show();
         $('.reg-box').hide();
     })
+    //登陆框
     const form = layui.form
+    const layer = layui.layer
     form.verify({
         pwd: [/^[\S]{6,12}$/, '密码必须6到12位，且不能出现空格'],
         repwd: function (value) {
@@ -19,25 +21,46 @@ $(function () {
             }
         }
     })
+    
+    //注册框
     $("#form_reg").on("submit", function (e) {
         e.preventDefault()
         $.ajax({
             method: "POST",
-            url: 'http://api-breakingnews-web.itheima.net/api/reguser',
+            url: '/api/reguser',
             data: {
                 username: $(".reg-box [name=username]").val(),
                 password: $(".reg-box [name=password]").val()
             },
             success: function (res) {
-                console.log(res);
-
                 if (res.status !== 0) {
                     return console.log(res.message);
                 }
-                console.log('注册成果');
+                layer.msg('注册成功')
+                $('#link_login').click()
 
             }
-
         })
     })
-})                          
+
+    //登陆跳转
+    $("#form_login").submit(function (e) {
+        e.preventDefault()
+        $.ajax({
+            type: "POST",
+            url: '/api/login',
+            data: $(this).serialize(),
+            success: function (res) {
+                console.log(res);
+                if (res.status !== 0) {
+                    return layer.msg('登陆失败')
+                }
+                layer.msg('登陆成功')
+                localStorage.setItem('token', res.token)
+                location.href = '/index.html'
+            }
+        })
+
+    })
+
+})
